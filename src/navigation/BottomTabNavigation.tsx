fd62c6aded35eff71aa407cad22c076cd18faa2d
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-03 11:27:48 +0400

    Added screen options for bottom tab navigation

--- src/navigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
 import { Home, Cart, Profile, Search, ProductDetails } from '../screens';
 import { Routes } from '../constants';
 
 const Tab = createBottomTabNavigator();
 
+const screenOptions: BottomTabNavigationOptions = {
+  tabBarShowLabel: false,
+  tabBarHideOnKeyboard: true,
+  headerShown: false,
+  tabBarStyle: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 70,
+  }
+}
+
 function BottomTabNavigation() {
   return (
-    <Tab.Navigator initialRouteName={Routes.HOME}>
+    <Tab.Navigator
+      initialRouteName={Routes.HOME}
+      screenOptions={screenOptions}
+    >
       <Tab.Screen name={Routes.HOME} component={Home} />
       <Tab.Screen name={Routes.CART} component={Cart} />
       <Tab.Screen name={Routes.SEARCH} component={Search} />
